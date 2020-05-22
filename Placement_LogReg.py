4b5e79f59a09155ce6b04c0e40927af643161a45
David Andres <andressanchezdavid@gmail.com>
2020-05-22 15:14:34 +0100

    compare results with Random Forests

--- Placement_LogReg.py
+++ Placement_LogReg.py
@@ -19,8 +19,11 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.pipeline import Pipeline
 from scipy import stats
 from sklearn.model_selection import cross_val_score
-from sklearn.linear_model import LogisticRegression, 
+from sklearn.linear_model import LogisticRegression 
 from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import scale
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 #%% Import data
 
@@ -56,6 +59,7 @@ data = pd.get_dummies(data, drop_first=True)
 
 # split of data into train and test
 X = data.drop(columns=['status_Placed'])
+X = scale(X)
 y = data.status_Placed
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
@@ -104,6 +108,7 @@ print("Best: %f using %s" % (logreg_cv.best_score_, logreg_cv.best_params_))
 
 #%% Hyperparameter tuning for K-Neighbors Classifier
 
+
 n_neigh=range(2,10,2)
 param_grid = dict(n_neighbors=n_neigh)
 
@@ -118,9 +123,7 @@ print("Best: %f using %s" % (knn_cv.best_score_, knn_cv.best_params_))
 
 #%% K-Neighbors Classifier
 
-from sklearn.neighbors import KNeighborsClassifier
-
-knn = KNeighborsClassifier(n_neighbors=8)
+knn = KNeighborsClassifier(n_neighbors=6)
 knn.fit(X_train, y_train)
 y_pred = knn.predict(X_test)
 
@@ -144,4 +147,45 @@ roc_auc_score(y_test, y_pred_prob)
 cv_scores = cross_val_score(logreg, X, y, cv=5, scoring='roc_auc')
 print(cv_scores)
 
+
+
+#%% Hyperparameter tuning for Random Forests
+
+n_estim=range(20,200,20)
+param_grid = dict(n_estimators=n_estim)
+
+# use GridSearchCV with KNeightbors and the chosen hyperparameters
+clf = RandomForestClassifier()
+clf_cv = GridSearchCV(clf, param_grid, cv=5)
+clf_cv.fit(X, y)
+
+# summary of results
+print("Best: %f using %s" % (clf_cv.best_score_, clf_cv.best_params_))
+
+
+#%% Random Forests Classifier
+
+clf=RandomForestClassifier(n_estimators=60)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+# evaluation of the model
+print('Accuracy: {}%'.format(round((y_test == y_pred).sum()/len(y_test)*100,2)))
+
+# plot ROC curve
+y_pred_prob = clf.predict_proba(X_test)[:,1]
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
+plt.plot([0, 1], [0, 1], 'k--')
+plt.plot(fpr, tpr, label='Logistic Regression')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Logistic Regression ROC Curve')
+plt.show()
+
+# get ROC_AUC score
+roc_auc_score(y_test, y_pred_prob)
+
+# cross validation
+cv_scores = cross_val_score(clf, X, y, cv=5, scoring='roc_auc')
+print(cv_scores)
 
